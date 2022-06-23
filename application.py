ced688e3b5d992a7aa03a26948b03302a7a38324
Arihant <awasthiarihant@gmail.com>
2022-06-23 12:55:01 +0530

    feat: Created create-url route

--- application.py
+++ application.py
@@ -1,7 +1,27 @@
-from flask import Flask, jsonify
+import json
+from flask import Flask, request, jsonify
+from helpers import generate_url_code
+from config import db
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    pass
+@app.route("/create-url", methods=["POST"])
+def create_url():
+    content_type = request.headers.get("Content-Type")
+
+    if content_type == "application/json":
+        data = request.json
+        long_url = data["long-url"]
+        url_code = generate_url_code()
+        short_url = f"https://localhost.com/{url_code}"
+
+        # Inserting long_url, url_code and short_url in the database
+        db.execute("INSERT INTO link (long_url, short_url, url_code) VALUES (:long_url, :short_url, :url_code)",
+                {"long_url": long_url, "short_url": short_url, "url_code": url_code})
+        db.commit()
+
+        response = {"message": "Done", "Code": "SUCCESS"}
+        return jsonify(response)
+    else:
+        resp = {"message": "Unsupported Media Type", "code": "FAILED"}
+        return jsonify(resp)
--- helpers.py
+++ helpers.py
@@ -1,7 +1,7 @@
 import random 
 
 # chars from [a-zA-Z0-9]
-CHARS = ['0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz']
+CHARS = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
 
 def generate_url_code():
     random_chars = random.choices(CHARS, k = 5)
