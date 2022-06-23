5f8cd6083d3d701a93e17155ef954a0397a14791
Arihant <awasthiarihant@gmail.com>
2022-06-23 14:52:21 +0530

    feat: Created Get url + error handling

--- application.py
+++ application.py
@@ -14,7 +14,13 @@ def create_url():
 
     if content_type == "application/json":
         data = request.json
-        long_url = data["long-url"]
+
+        # Handling Key Error
+        try:
+            long_url = data["long-url"]
+        except:
+            abort(500)
+
         url_code = generate_url_code()
         short_url = f"https://localhost.com/{url_code}"
 
@@ -23,7 +29,7 @@ def create_url():
                 {"long_url": long_url, "short_url": short_url, "url_code": url_code})
         db.commit()
 
-        response = {"message": "Done", "Code": "SUCCESS"}
+        response = {"message": "Done", "Code": "SUCCESS", "short_url": short_url}
         return jsonify(response)
     else:
         resp = {"message": "Unsupported Media Type", "code": "FAILED"}
@@ -37,8 +43,19 @@ def update_url():
 
     if content_type == "application/json":
         data = request.json
-        short_url = data["short-url"]
-        new_long_url = data["new-long-url"]
+
+        # Handling Key Error
+        try:
+            short_url = data["short-url"]
+            new_long_url = data["new-long-url"]
+        except:
+            abort(500)
+
+        # Checking if short_url exists throwing 404 if do no exist
+        query = db.execute("SELECT short_url FROM link WHERE short_url=:short_url",
+                {"short_url": short_url}).fetchone()
+        if not query:
+            abort(404)
 
         # Updating new long url provided by the client
         db.execute("UPDATE link SET long_url=:long_url WHERE short_url=:short_url", 
@@ -55,7 +72,7 @@ def update_url():
 # For Deleting an existing short url
 @app.route("/delete-url/<url_code>", methods=['DELETE'])
 def delete_url(url_code):
-    # Checking if url_code exists
+    # Checking if url_code exists throwing 404 if do no exist
     query = db.execute("SELECT url_code FROM link WHERE url_code=:url_code",
             {"url_code": url_code}).fetchone()
     if not query:
@@ -68,6 +85,30 @@ def delete_url(url_code):
     return jsonify({"message": "URL Deleted", "code": "Success"})
 
 
+# For retrieving information about the long url
+@app.route("/get-url", methods=['GET'])
+def get_url():
+    #Handling Key Error
+    try:
+        short_url = request.args.get("short-url")
+    except:
+        abort(500)
+
+    # retrieving info regarding short url provided
+    data = db.execute("SELECT long_url, url_code, created_at FROM link WHERE short_url=:short_url",
+            {"short_url": short_url}).fetchone()
+    print(data)
+    
+    # throwing 404 exception if data does not exist
+    if not data:
+        abort(404)
+
+    (long_url, url_code, created_at) = data
+
+    return jsonify({"long_url": long_url, "url_code": url_code, "created_at": created_at})
+
+
+
 @app.errorhandler(404)
 def not_found(e):
     return jsonify({"message": "FAILURE", "type": "404 Not Found"})
@@ -76,9 +117,6 @@ def not_found(e):
 def internal_server_error(e):
     return jsonify({"message": "FAILURE", "type": "Internal Server Error"})
 
-@app.route("/")
-def index():
-    new_list = db.execute("SELECT * FROM link").fetchall()
-    for i in new_list:
-        print(i)
-    return "Hello"
+@app.errorhandler(405)
+def method_not_allowed(e):
+    return jsonify({"message": "FAILURE", "type": "Method Not Allowed"})
